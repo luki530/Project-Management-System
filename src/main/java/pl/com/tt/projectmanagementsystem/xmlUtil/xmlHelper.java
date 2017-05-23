8857d1d4061b03bb88870d17fb0f7bd0ea4b1e89
Lukasz <lukasz.oszczypala@gmail.com>
2017-05-23 08:59:44 +0200

    xml add

--- src/main/java/pl/com/tt/projectmanagementsystem/xmlUtil/xmlHelper.java
+++ src/main/java/pl/com/tt/projectmanagementsystem/xmlUtil/xmlHelper.java
@@ -10,51 +10,42 @@ import javax.xml.bind.Unmarshaller;
 
 public class XmlHelper {
 
-	private static final String RESOURCE_NAME = "Permissions.xml";
+    private static final String RESOURCE_NAME = "Permissions.xml";
+    
+    public List<String> getPermissions(String role) {
+        List<String> permissions = new ArrayList<>();
+        RolesPermissions rolesPermissions = getRolesPermissionsFromResource();
+        //TODO: rest of method logic
+        return permissions;
+    }
 
-	public static List<String> getPermissions(List<String> userRoles) {
-		List<String> permissions = new ArrayList<>();
-		RolesPermissions rolesPermissions = getRolesPermissionsFromResource();
-		List<Role> roles = rolesPermissions.getRoles();
-		for (String s : userRoles) {
-			for (Role r : roles) {
-				if (r.getName().equals(s)) {
-					for (String action : r.getActions()) {
-						permissions.add(action);
-					}
-				}
-			}
-		}
-		return permissions;
-	}
+    private RolesPermissions getRolesPermissionsFromResource() {
+        RolesPermissions rolesPermissiosn = new RolesPermissions();
+        try {
+            rolesPermissiosn = unmarshallRolessPermissions();
+        } catch (JAXBException e) {
+            //TODO: handle exception
+        }
+        return rolesPermissiosn;
+    }
+    
+    private RolesPermissions unmarshallRolessPermissions() throws JAXBException {
+        InputStream resourceInputStream = createResourceInputStream();
+        Unmarshaller jaxbUnmarshaller = createJaxbUnmarshaller();
+        return (RolesPermissions) jaxbUnmarshaller.unmarshal(resourceInputStream);
+    }
 
-	private static RolesPermissions getRolesPermissionsFromResource() {
-		RolesPermissions rolesPermissiosn = new RolesPermissions();
-		try {
-			rolesPermissiosn = unmarshallRolessPermissions();
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
-		return rolesPermissiosn;
-	}
-
-	private static RolesPermissions unmarshallRolessPermissions() throws JAXBException {
-		InputStream resourceInputStream = createResourceInputStream();
-		Unmarshaller jaxbUnmarshaller = createJaxbUnmarshaller();
-		return (RolesPermissions) jaxbUnmarshaller.unmarshal(resourceInputStream);
-	}
-
-	private static InputStream createResourceInputStream() {
-		return ClassLoader.getSystemResourceAsStream(RESOURCE_NAME);
-	}
-
-	private static Unmarshaller createJaxbUnmarshaller() throws JAXBException {
-		JAXBContext jaxbContext = createJAXBContext();
-		return jaxbContext.createUnmarshaller();
-	}
-
-	private static JAXBContext createJAXBContext() throws JAXBException {
-		return JAXBContext.newInstance(RolesPermissions.class);
-	}
+    private InputStream createResourceInputStream() {
+        return ClassLoader.getSystemResourceAsStream(RESOURCE_NAME);
+    }
+    
+    private Unmarshaller createJaxbUnmarshaller() throws JAXBException {
+        JAXBContext jaxbContext = createJAXBContext();
+        return jaxbContext.createUnmarshaller();
+    }
+    
+    private JAXBContext createJAXBContext() throws JAXBException {
+        return JAXBContext.newInstance(RolesPermissions.class);
+    }
 
 }
